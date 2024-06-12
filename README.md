36a574d38159e85b9f3087c06b26a5171f867f0e
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-06-12 08:40:08 +0100

    Add source icon, favicon, tweak style, update README

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 
 <!-- badges: start -->
 [![Project Status: Concept – Minimal or no implementation has been done yet, or the repository is only intended to be a limited example, demo, or proof-of-concept.](https://www.repostatus.org/badges/latest/concept.svg)](https://www.repostatus.org/#concept)
-[![](https://img.shields.io/badge/Shiny-shinylive-blue?style=flat&labelColor=white&logo=RStudio&logoColor=blue)](https://matt-dray.github.io/cran-deadlines/)
+[![](https://img.shields.io/badge/Shiny-shinyapps.io-blue?style=flat&labelColor=white&logo=RStudio&logoColor=blue)](https://mattdray.shinyapps.io/cran-deadlines/)
 <!-- badges: end -->
 
-A simple Shinylive app that highlights R packages on CRAN that are currently nearing their issue-response deadlines, according to `tools::CRAN_package_db()`. 
+A simple Shiny app that highlights R packages on [CRAN](https://cran.r-project.org/) that are currently nearing their archival deadlines, according to `tools::CRAN_package_db()`. 
 
-The app is available on the web but computations run in your browser: [matt-dray.github.io/cran-deadlines](https://matt-dray.github.io/cran-deadlines)
+The app is available on shinyapps.io for now: [matt-dray.github.io/cran-deadlines](https://mattdray.shinyapps.io/cran-deadlines/)
 
-Built primarily with [Shiny](https://shiny.posit.co/r/getstarted/shiny-basics/lesson1/index.html), [{bslib}](https://rstudio.github.io/bslib/), and [{shinylive}](https://posit-dev.github.io/r-shinylive/).
+Built primarily with [Shiny](https://shiny.posit.co/r/getstarted/shiny-basics/lesson1/index.html) and [{bslib}](https://rstudio.github.io/bslib/), with many thanks to the CRAN team and all package maintainers.
+
+![](app.png){fig-alt="Screenshot of webpage called 'CRAN Deadlines'. There are individual cards with the names of R packages in them. The header of each card says how many days remain for fixes to be made and is coloured according to the amount of time left (e.g. red for 0 days). The footer says e.g. 'issues need fixing before 2024-06-12' with a link to the CRAN page detailing the errors."}
--- app/app.R
+++ app/app.R
@@ -1,75 +1,83 @@
 colour_days <- function(value) {
-    if (value < 2) return("bg-red")
-    if (value %in% 2:7) return("bg-orange")
-    "bg-yellow"
+  if (value < 2) return("bg-red")
+  if (value %in% 2:7) return("bg-orange")
+  "bg-yellow"
 }
 
 pluralise <- function(value) if (value == 1) "day" else "days"
 
 ui <- bslib::page_fillable(
-    theme = bslib::bs_theme(preset = "bootstrap"),
-    padding = 100,
-    fillable_mobile = TRUE,
-    title = "CRAN Deadlines",
-    bslib::input_dark_mode(id = "dark_mode", mode = "dark"),
-    htmltools::h1("⌛️ CRAN Deadlines"),
-    shiny::htmlOutput("cards", inline = TRUE)
+  htmltools::HTML(r"{<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⌛️</text></svg>">}"), # https://twitter.com/LeaVerou/status/1241619866475474946
+  theme = bslib::bs_theme(preset = "bootstrap"),
+  padding = 75,
+  fillable_mobile = TRUE,
+  title = "CRAN Deadlines",
+  shiny::column(
+    width = 12,
+    htmltools::a(
+      href = "https://github.com/matt-dray/cran-deadlines",
+      fontawesome::fa("github", height = "1.45em")
+    ),
+    bslib::input_dark_mode(id = "dark_mode", mode = "dark")
+  ),
+  htmltools::h1("⌛️ CRAN Deadlines"),
+  shiny::htmlOutput("cards", inline = TRUE)
 )
 
 server <- function(input, output, session) {
 
-    db <- tools::CRAN_package_db()
-    db <- db[!is.na(db$Deadline), c("Deadline", "Package", "Title")]
-    db$URL <- paste0("https://CRAN.R-project.org/package=", db$Package)
-    db$Deadline <- as.Date(db$Deadline)
-    db$Days <- db$Deadline - Sys.Date()
-    db <- db |> sort_by(~list(Deadline, Package))
+  db <- tools::CRAN_package_db()
+  db <- db[!is.na(db$Deadline), c("Deadline", "Package", "Title")]
+  db$URL <- paste0("https://CRAN.R-project.org/package=", db$Package)
+  db$Deadline <- as.Date(db$Deadline)
+  db$Days <- db$Deadline - Sys.Date()
+  db <- db |> sort_by(~list(Deadline, Package))
 
-    cards_list <- vector("list", length = nrow(db))
+  cards_list <- vector("list", length = nrow(db))
 
-    for (i in seq(nrow(db))) {
+  for (i in seq(nrow(db))) {
 
-        pkg <- db[i, ]
+    pkg <- db[i, ]
 
-        cards_list[[i]] <- bslib::card(
-            bslib::card_header(
-                class = colour_days(pkg$Days),
-                paste(pkg$Days, pluralise(pkg$Days))
-            ),
-            bslib::card_title(
-                htmltools::a(
-                    href = pkg$URL,
-                    paste0("{", pkg$Package, "}"),
-                    .noWS = "after"
-                ),
-                ":", pkg$Title
-            ),
-            bslib::card_footer(
-                htmltools::a(
-                    href = paste0(
-                        "https://cran.r-project.org/web/checks/check_results_",
-                        pkg$Package,
-                        ".html"
-                    ),
-                    "Issues"
-                ),
-                "need fixing before",
-                pkg$Deadline
-            )
-        )
-    }
+    cards_list[[i]] <- bslib::card(
+      bslib::card_header(
+        class = colour_days(pkg$Days),
+        paste(pkg$Days, pluralise(pkg$Days))
+      ),
+      bslib::card_title(
+        htmltools::a(
+          href = pkg$URL,
+          paste0("{", pkg$Package, "}"),
+          .noWS = "after"
+        ),
+        ":", pkg$Title
+      ),
+      bslib::card_footer(
+        htmltools::a(
+          href = paste0(
+            "https://cran.r-project.org/web/checks/check_results_",
+            pkg$Package,
+            ".html"
+          ),
+          "Issues"
+        ),
+        "need fixing before",
+        pkg$Deadline
+      )
+    )
+  }
 
-    cards_taglist <- htmltools::tagList()
+  cards_taglist <- htmltools::tagList()
 
-    for (i in seq_along(cards_list)) {
-        cards_taglist <- cards_taglist |>
-            htmltools::tagAppendChild(cards_list[[i]]) |>
-            htmltools::tagAppendChild(htmltools::br())
-    }
+  for (i in seq_along(cards_list)) {
+    cards_taglist <- cards_taglist |>
+      htmltools::tagAppendChild(cards_list[[i]]) |>
+      htmltools::tagAppendChild(htmltools::br())
+  }
 
-    output$cards <- shiny::renderUI({
-        cards_taglist
-    })
+  output$cards <- shiny::renderUI({
+    cards_taglist
+  })
 
 }
 
